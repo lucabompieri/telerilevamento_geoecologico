8db6a6fc547ef1528e0ff9859f1355d204411a9c
lucabompieri <100956834+lucabompieri@users.noreply.github.com>
2022-08-14 21:36:15 +0200

    Create 07_R_code_variability.r

---
+++ 07_R_code_variability.r
@@ -0,0 +1,47 @@
+# this is the 7th script
+# R_code_variability.r
+
+# upload "raster" package
+library(raster)
+# upload "ggplo2"
+library(ggplot2)
+# upload "patchwork"
+library(patchwork)
+# install "viridis" package
+install.package("viridis")
+library(viridis)
+
+# set working directory
+setwd("C:/lab/")
+
+# use the function brick to import the RGB image
+sen <- brick("sentinel.png")
+sen
+
+# plot the image with the function "ggRGB"
+ggRGB(sen, 1, 2, 3, stretch="lin")
+# this function put stretch = lin  in auto
+ggRGB(sen, 1, 2, 3)
+dev.off()
+ggRGB(sen, 2, 1, 3)
+
+g1 <- ggRGB(sen, 1, 2, 3)
+g2 <, ggRGB(sen, 2, 1, 3)
+g1+g2
+
+# calculate the variability oth the image
+# calculate it on NIR, the first element
+nir <- sen[[1]]
+# to make the calc, use the function "focal" on a 3*3 pixel window
+sd3 <- focal(nir, matrix(1/9, 3, 3), fun=sd)
+
+# plot the variability image with an appropriate Color Palette
+clsd <- colorRampPalette(c("blue","green","pink","magenta","orange","brown","red","yellow"))(100)
+plot(sd3, col=clsd)
+dev.off()
+
+# plot with "ggplot" function
+# open an empty plot and add to it the function "geom_raster"
+
+
+
